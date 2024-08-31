6237904d527fd21cb13ea8a796ca8de1235868bb
NemanjaCyber <neca.s@elfak.rs>
2024-08-31 19:50:22 +0200

    AutoMapper added, Index action changet to mapper functionality

--- LeaveManagementSystem.Web/Controllers/LeaveTypesController.cs
+++ LeaveManagementSystem.Web/Controllers/LeaveTypesController.cs
@@ -6,22 +6,36 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using LeaveManagementSystem.Web.Data;
+using LeaveManagementSystem.Web.Models.LeaveTypes;
+using AutoMapper;
 
 namespace LeaveManagementSystem.Web.Controllers
 {
     public class LeaveTypesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
 
-        public LeaveTypesController(ApplicationDbContext context)
+        public LeaveTypesController(ApplicationDbContext context, IMapper mapper)
         {
             _context = context;
+            this._mapper = mapper;
         }
 
         // GET: LeaveTypes
         public async Task<IActionResult> Index()
         {
-            return View(await _context.LeaveTypes.ToListAsync());
+            var data = await _context.LeaveTypes.ToListAsync();
+            //convert datamodel to a viewmodel ILI use AutoMapper
+            //var viewData = data.Select(q => new IndexVM
+            //{
+            //    Id = q.Id,
+            //    Name = q.Name,
+            //    NumberOfDays = q.NumberOfDays,
+            //});
+            var viewData=_mapper.Map<List<IndexVM>>(data);//ovaj mapper nam automatski radi ovo sto je iznad zakomentarisano
+            //return a viewmodel to a view
+            return View(viewData);
         }
 
         // GET: LeaveTypes/Details/5
